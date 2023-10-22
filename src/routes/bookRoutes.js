e498479cbd96e11e029c0cc365f858a1252d8fe1
Nazaré Almeida <126839545+nazare4lmeida@users.noreply.github.com>
2023-10-22 08:20:51 -0300

    Projeto Fullstack

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bookController = require("../controllers/bookController");
+const bookController = require("../controllers/BookController");
 
 // Listar todos os livros
 router.get("/", bookController.getBooks);
