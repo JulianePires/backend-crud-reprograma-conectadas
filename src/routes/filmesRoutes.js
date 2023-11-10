b932a3ec6b22f53bd2850a927f482a16a9c7327d
Nazaré Almeida <126839545+nazare4lmeida@users.noreply.github.com>
2023-11-09 23:56:18 -0300

    Projeto Fullstack

--- src/routes/filmesRoutes.js
+++ src/routes/filmesRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const filmeController = require("../controllers/FilmesController");
+const filmeController = require("../controllers/FilmesController")
 
 // Listar todos os filmes
 router.get("/", filmeController.getFilmes);
